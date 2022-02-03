5b71e3f72f6bada17c44b809baaca92f98b5ba3f
abdullahesmael <abdullahesmael98@gmail.com>
2022-02-03 08:01:50 +0200

    update Project to solc v0.8.9

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,21 +1,25 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const Web3 = require("web3");
-const {interface, bytecode} = require("./compile");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
 
-const provider = new HDWalletProvider(
-    'pave afraid tennis amazing concert toe dog explain glimpse asthma obscure abandon', // replace mnemonic with your own
-    'https://rinkeby.infura.io/v3/0acfebeade894b578dd109dcd0662137' // replace rinkeby with your own network
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'REPLACE_WITH_YOUR_MNEMONIC',
+  'REPLACE_WITH_YOUR_INFURA_URL'
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
-    console.log('Attempting to deploy from account', accounts[0]);
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi there']})
-        .send({gas: '1000000', from: accounts[0]});
-    
-    console.log('Contract deployed to', result.options.address);
-    provider.engine.stop();
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
 };
 deploy();
